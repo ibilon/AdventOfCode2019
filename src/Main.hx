1e82bd151b55d60db63ce568fe518318f10a1353
Valentin Lemière <valentin@ibilon.com>
2019-12-06 14:37:52 +0100

    Day 6

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,7 @@
 class Main {
 	static function main() {
-		Sys.println("Day05");
-		Sys.println('part 1: ${days.Day05.part1()}');
-		Sys.println('part 2: ${days.Day05.part2()}');
+		Sys.println("Day06");
+		Sys.println('part 1: ${days.Day06.part1()}');
+		Sys.println('part 2: ${days.Day06.part2()}');
 	}
 }
---
+++ src/days/Day06.hx
@@ -0,0 +1,55 @@
+package days;
+
+import sys.io.File;
+
+typedef OrbitMap = Map<String, String>;
+
+class Day06 {
+	static function pathToCom(map:OrbitMap, obj:String):Array<String> {
+		var path = [];
+		var p;
+
+		do {
+			p = map.get(obj);
+			obj = p;
+			path.push(p);
+		} while (p != "COM");
+
+		return path;
+	}
+
+	static function orbitMap():OrbitMap {
+		var map = new OrbitMap();
+
+		for (orbit in File.getContent("data/day06.txt").split("\n")) {
+			var orbit = orbit.split(")");
+			map.set(orbit[1], orbit[0]);
+		}
+
+		return map;
+	}
+
+	public static function part1():Int {
+		var map = orbitMap();
+		var total = 0;
+
+		for (obj in map.keys()) {
+			total += pathToCom(map, obj).length;
+		}
+
+		return total;
+	}
+
+	public static function part2():Int {
+		var map = orbitMap();
+		var you = pathToCom(map, "YOU");
+		var san = pathToCom(map, "SAN");
+
+		while (you[you.length - 1] == san[san.length - 1]) {
+			you.pop();
+			san.pop();
+		}
+
+		return you.length + san.length;
+	}
+}
