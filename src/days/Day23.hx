13f0a4bb53c8f4a74af1298b03e7f5da2666510f
Valentin Lemière <valentin@ibilon.com>
2019-12-23 11:50:58 +0100

    Day 23 part 2

--- src/days/Day23.hx
+++ src/days/Day23.hx
@@ -1,10 +1,13 @@
 package days;
 
 class Day23 {
-	public static function part1():Int {
+	static function run(is_part1:Bool):Int {
 		var memory = IntCodeVM.loadProgram("data/day23.txt");
 		var network = [];
 		var queue = [];
+		var nat = [-1.0, -1.0];
+		var nat_history0 = -1.0;
+		var nat_history1 = -1.0;
 
 		for (i in 0...50) {
 			network.push(new IntCodeVM(memory, [i]));
@@ -12,6 +15,8 @@ class Day23 {
 		}
 
 		while (true) {
+			var idle = true;
+
 			for (i in 0...50) {
 				var c = network[i];
 
@@ -20,15 +25,22 @@ class Day23 {
 				} else {
 					var msg = queue[i].shift();
 					c.inputs(msg);
+					idle = false;
 				}
 
 				while (true) {
 					switch ([c.output(), c.output(), c.output()]) {
-						case [Some(y), Some(_), Some(255)]:
-							return Std.int(y);
+						case [Some(y), Some(x), Some(255)]:
+							if (is_part1) {
+								return Std.int(y);
+							}
+							nat[0] = x;
+							nat[1] = y;
+							idle = false;
 
 						case [Some(y), Some(x), Some(a)]:
 							queue[Std.int(a)].push([x, y]);
+							idle = false;
 
 						case [WaitingForInput, WaitingForInput, WaitingForInput]:
 							break;
@@ -38,12 +50,24 @@ class Day23 {
 					}
 				}
 			}
-		}
 
-		return 0;
+			if (idle) {
+				queue[0].push(nat);
+				nat_history0 = nat_history1;
+				nat_history1 = nat[1];
+
+				if (nat_history0 == nat_history1) {
+					return Std.int(nat_history0);
+				}
+			}
+		}
+	}
+
+	public static function part1():Int {
+		return run(true);
 	}
 
 	public static function part2():Int {
-		return 0;
+		return run(false);
 	}
 }
